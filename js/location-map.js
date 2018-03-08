ea3bdabc01f5f1e6c8c262472c726634b6fcae1a
Aleksandr Chupakhin <79205937997@ya.ru>
2018-03-08 11:12:03 +0300

    Подключена карта с меткой

---
+++ js/location-map.js
@@ -0,0 +1,27 @@
+ymaps.ready(function() {
+  var myMap = new ymaps.Map(
+    'location-map',
+    {
+      center: [59.938631, 30.323055],
+      zoom: 16,
+      controls: ['smallMapDefaultSet']
+    },
+    {
+      minZoom: 10,
+      yandexMapDisablePoiInteractivity: true
+    }
+  );
+  myMap.behaviors.disable('scrollZoom');
+
+  var myPlacemark = new ymaps.Placemark(
+    [59.938631, 30.323055],
+    {},
+    {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map-pin.png',
+      iconImageSize: [218, 142],
+      iconImageOffset: [-40, -140]
+    }
+  );
+  myMap.geoObjects.add(myPlacemark);
+});
