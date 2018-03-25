ae139bf7e2cc5d6d1a42a92f269670e5ecc89c1f
Aleksandr Chupakhin <79205937997@ya.ru>
2018-03-25 14:08:33 +0300

    Доработка окна фидбека и карты

--- js/feedback-modal.js
+++ js/feedback-modal.js
@@ -1,7 +1,37 @@
-function toggleFeedbackWindow () {
-  document.querySelector('.feedback').classList.toggle('feedback_shown');
+function initFeedback() {
+  var feedbackWindow = document.querySelector('.feedback');
+
+  document.getElementById('feedback-button')
+    .addEventListener('click', showFeedback);
+
+  [
+    document.querySelector('.feedback__close-btn'), 
+    document.querySelector('.feedback__mask-overlay')
+  ]
+    .forEach(function (element) {
+      element.addEventListener('click', hideFeedback);
+    });
+
+  function showFeedback() {
+    if (!feedbackWindow.classList.contains('feedback_shown')) {
+      document.addEventListener('keydown', onKeyPress);
+      feedbackWindow.classList.add('feedback_shown');
+    }
+  }
+
+  function hideFeedback() {
+    if (feedbackWindow.classList.contains('feedback_shown')) {
+      document.removeEventListener('keydown', onKeyPress);
+      feedbackWindow.classList.remove('feedback_shown');
+    }
+  }
+
+  function onKeyPress (e) {
+    if (e.keyCode == 27) {
+      hideFeedback();
+    }
+  }
 }
 
-[document.getElementById('feedback-button'), document.querySelector('.feedback__close-btn')].forEach(function(element) {
-  element.addEventListener('click', toggleFeedbackWindow);
-})
+
+initFeedback();
